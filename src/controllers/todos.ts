7810799a3ebca64a1f6f245a0f34275f46024767
takumi_kurogi <takumines1@gmail.com>
2022-05-25 19:40:22 +0900

    取得・更新・削除処理を追加

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -13,3 +13,31 @@ export const createTodo: RequestHandler = (req, res, _next) => {
 
   res.status(201).json({message: 'TODOを作成しました。', createdTodo: newTodo});
 }
+
+export const getTodos: RequestHandler = (_req, res, _next) => {
+  res.json({todos: TODOS});
+}
+
+// ジェネリクスでRequestHandlerのparams内のidがstring型であることを指定する
+export const updateTodo: RequestHandler<{id: string}> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updateText = (req.body as {text: string}).text;
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error("対象のTODOが見つかりませんでした");
+  }
+  TODOS[todoIndex] = new Todo(todoId, updateText);
+
+  res.status(201).json({message: "更新しました。", updatedTodo: TODOS[todoIndex]})
+}
+
+export const deleteTodo: RequestHandler<{id: string}> = (req, res, _next) => {
+  const todoId = req.params.id;
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error("対象のTODOが見つかりませんでした");
+  }
+  TODOS.splice(todoIndex, 1);
+
+  res.status(200).json({message: "TODOを削除しました。"})
+}
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createTodo } from '../controllers/todos';
+import { createTodo, getTodos, updateTodo, deleteTodo } from '../controllers/todos';
 
 const router = Router();
 
 router.post('/', createTodo);
-router.get('/');
-router.patch('/');
-router.delete('/');
+router.get('/', getTodos);
+router.patch('/:id', updateTodo);
+router.delete('/:id', deleteTodo);
 
 export default router;
