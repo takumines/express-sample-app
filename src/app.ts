cfa2d74b50a1bcb74e4882ff61d2d646985f949b
takumi_kurogi <takumines1@gmail.com>
2022-05-25 18:51:20 +0900

    ルーティングとミドルウェア作成

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import todoRoute from './router/todos';
 
 const app = express();
+
+app.use('/todos', todoRoute);
+app.use((err: Error, req:Request, res:Response, next:NextFunction) => {
+  res.status(500).json({message: err.message});
+});
 
 app.listen(3000);
---
+++ src/router/todos.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post('/');
+router.get('/');
+router.patch('/');
+router.delete('/');
+
+export default router;
